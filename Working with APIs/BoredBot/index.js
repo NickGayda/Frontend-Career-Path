017369f2afdc5f92034b2d73da3450652911fe64
NickGayda <54640052+NickGayda@users.noreply.github.com>
2023-09-14 21:06:57 -0500

    Add files via upload

---
+++ Working with APIs/BoredBot/index.js
@@ -0,0 +1,7 @@
+document.getElementById('activity-button').addEventListener('click', () => {
+    fetch('https://apis.scrimba.com/bored/api/activity')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('activity').textContent = data.activity
+        })
+})
