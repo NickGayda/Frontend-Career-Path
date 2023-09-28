60424c3f43383f4804f67f3b9df827793410ecc1
NickGayda <54640052+NickGayda@users.noreply.github.com>
2023-09-27 20:29:33 -0500

    Add files via upload

---
+++ React Basics/Digital Business Card/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="content">
+            <h2>About</h2>
+            <p>I am a Computer Engineer that graduated from Purdue University. I want to eventually become a Full-stack Developer, but I am starting off by learning Frontend Development.</p>
+        </div>
+    )
+}
---
+++ React Basics/Digital Business Card/components/Interests.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Interests() {
+    return (
+        <div className="content">
+            <h2>Interests</h2>
+            <p>I am interested in computers, coding of any sort, and working out. A typical day for me outside of work contains a workout and working on some sorting of coding project.</p>
+        </div>
+    )
+}
